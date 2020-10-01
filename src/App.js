a596a541f4b33bd8d68c802b5f2c475943ba08d5
Cloja <caloja@espol.edu.ec>
2020-10-01 10:57:08 -0500

    Fixed Cuntry Card Errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { CountryCard } from './components/CountryCard';
 import SearchBar from './components/SearchBar'
 
 function App() {
   return (
     <div className="App-content">
       <SearchBar></SearchBar>
+      
+
     </div>
   );
 }
--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Card, Space } from "antd";
 
 
-export function CountryCard(pais,productos){
+export function CountryCard({pais,productos}){
+   
     return (
       <div className="site-card-border-less-wrapper">
         <Card title={pais} bordered={false} style={{ width: 300 }}>
@@ -9,9 +11,9 @@ export function CountryCard(pais,productos){
             <p>
               <div className="space-align-block">
                 <Space align="center">
-                  <img src={producto.image} height={50}/>
+                  <img src={producto.image} height={50} />
                   {producto.name}
-                  <img src={producto.grupo} height={50}/>
+                  <img src={producto.grupo} height={50} />
                 </Space>
               </div>
             </p>
