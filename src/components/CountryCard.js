85e22cd8767cfe0812540b93b0b7c48985d53a2b
Cloja <caloja@espol.edu.ec>
2020-10-04 15:16:09 -0500

    last commit

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,6 +1,7 @@
-import React , {useEffect,useState} from 'react';
+import React , {useEffect,useState,useRef} from 'react';
 import { Card, Space, Spin } from "antd";
 import { LoadingOutlined } from '@ant-design/icons';
+import InfiniteScroll from 'react-infinite-scroll-component'
 import Axios from 'axios';
 
 let useProducts = (setProducts, country) => {
@@ -34,44 +35,80 @@ function _getCleanProducts(productos){
 export function CountryCard({pais }){
   const [products,setProducts]= useState([])
   useProducts(setProducts,pais);
+  let state={
+    items: Array.from({
+      length:4,
+      hasMore:true
+    })
+  }
+  let fetchMoreData =()=>{
+    console.log("1")
+    if(state.items.length>=products.length-1){
+      state.hasMore = false;
+    }
+      setTimeout(
+        ()=>state.items.concat(Array.from({ length: 4 }))
+          
+        ,500
+      )
+  }
   var producto = products.length==0 ? (
     <Spin
       indicator={<LoadingOutlined style={{ fontSize: 24 }}></LoadingOutlined>}
     ></Spin>
   ) : (
-    _getCleanProducts(products).map((producto) => {
-      let imagen = producto["image_url"]
-        ? producto["image_url"]
-        : "https://i.postimg.cc/jqzkmB2H/shop-01.jpg";
-        console.log(producto)
-      return (
-        <p>
-          <Card style={{margin:"10px", height:98,background:"#e39f9f"}} >
-            <div className="space-align-block">
-              <Space align="center" >
-                <img src={imagen} height={50} width={50}/>
-                <div style={{fontSize:"0.75vw" } }>{producto.product_name}</div>
-                {producto.nova_group && (
-                  <img
-                    src={`https://static.openfoodfacts.org/images/misc/nova-group-${producto.nova_group}.svg`}
-                    height={50}
-                    width={50}
-                  />
-                )}
-                
-              </Space>
-            </div>
-          </Card>
-        </p>
-      );
-    })
+    <div 
+    id ="scrallabelDiv"
+    style={{
+      height:350,
+      overflow: 'auto',
+      display:'flex',
+      flexDirection:'column-reverse'
+    }}
+    >
+      <InfiniteScroll 
+      dataLength={products.length-1}
+      next={fetchMoreData}
+      hasMore={state.hasMore}
+      loader={<h4>Loading...</h4>}
+      
+      >
+        {
+          products.map((producto) => {
+            let imagen = producto["image_url"]
+              ? producto["image_url"]
+              : "https://i.postimg.cc/jqzkmB2H/shop-01.jpg";
+              console.log(producto)
+            return (
+              <p>
+                <Card style={{margin:"10px", height:98,background:"#e39f9f"}} >
+                  <div className="space-align-block">
+                    <Space align="center" >
+                      <img src={imagen} height={50} width={50}/>
+                      <div style={{fontSize:"0.75vw" } }>{producto.product_name}</div>
+                      {producto.nova_group && (
+                        <img
+                          src={`https://static.openfoodfacts.org/images/misc/nova-group-${producto.nova_group}.svg`}
+                          height={50}
+                          width={50}
+                        />
+                      )}
+                      
+                    </Space>
+                  </div>
+                </Card>
+              </p>
+            );
+          })
+        }
+      </InfiniteScroll>
+    </div>
   );
     return (
       <div className="site-card-border-less-wrapper">
         <Card title={pais} bordered={false} style={{ width: 300 ,height:450,background:"#e39f9f"}}>
           {producto
              }
-             Ver mas
         </Card>
       </div>
     );
